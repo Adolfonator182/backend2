00931812336a84ddc367c4c94a282c662717cb5d
Adolfonator182 <a620014428@my.uvm.edu.mx>
2024-12-05 14:06:09 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -1,101 +1,118 @@
-require('dotenv').config()
+require ('dotenv').config()
+
 const express = require('express')
 const cors = require('cors')
 const app = express()
-const Note = require('./models/note')
+const Contact = require('./models/contacto.js')
 
 app.use(express.json()) //obtener datos que envia el usuario
-app.use(cors())
+app.use(cors()) //para poder pedir o enviar datos desde otros servidores diferentes al alojados
 app.use(express.static('dist'))
 
-const requestLogger = (request, response, next) => {
-    console.log('Method', request.method);
-    console.log('Path', request.path);
-    console.log('Body', request.body);
-    console.log('---');
-    next()
-}
 
-app.use (requestLogger)
+//Mildware-------------------------------------------------------------------------
+const morgan = require('morgan')
+morgan.token('body', (req) => JSON.stringify(req.body))
+app.use((req, res, next) => {
+  const format = req.method === 'POST'
+    ? ':method :url :status :res[content-length] - :response-time ms :body'
+    : 'tiny';
+  morgan(format)(req, res, next);
+})
+//-----------------------------------------------------------------------------------
 
-let notes = [
+let persons = [
         {
         id: 1,
-        content: "HTML is easy",
-        important: true
+        name: "Jose Alberto",
+        number: 9931798565
         },
         {
-        id: 2,
-        content: "Browser can execute only javascript",
-        important: false
+            id: 2,
+            name: "Adolfo",
+            number: 9933225858
         },
         {
-        id: 3,
-        content: "GET and POST are the most important methods of HTTP Protocol",
-        important: true
+            id: 3,
+            name: "Carlos",
+            number: 9931307185
         }
     ]
-
+ 
     app.get('/',(request,response) => {
-        response.send('<h1>API REST FROM NOTES</h1>')
+        response.send('<h1>API REST FROM AGENDA</h1>')
     })
-    app.get('/api/notes',(request,response) => {
-        Note.find({}).then(notes =>{
-            response.json(notes)
+    app.get('/api/persons',(request,response) => {
+        Contact.find({}).then(persons =>{
+            response.json(persons)
             })
     })
-
-    app.get('/api/notes/:id',(request,response) => {
-        Note.findById(request.params.id)
-            .then( note => {
-                if(note){
-                    response.json(note)
-                }
-                else{
-                    response.status(404).end()
-                }
-            })
-            .catch(error => {
-                console.log(error);
-                response.status(400).send({error: 'malformated id'})
-            })
-    })
-
-    app.delete('/api/notes/:id',(request,response) => {
-        const id = Number(request.params.id)
-        //console.log('Delete id:',id);
-        notes=notes.filter(n => n.id !== id)
-        response.status(204).end()
-    })
-
-    app.post('/api/notes',(request,response) => { //agrega notas
-        const body = request.body
-        if (!body.content){
-            return response.status(400).json({
-                error: 'Content missing'
-            })
-        }
-        const note = new Note( {
-            content: body.content,
-            important: body.important || false            
+    app.get('/api/persons/:id',(request,response) => {
+        Contact.findById(request.params.id)
+        .then (person =>{
+            if(person) {
+                response.json(person)
+            }
+            else {
+                response.status(404).end()
+            }
         })
-        note.save().then(result => response.json(note))
+        .catch (error => {
+            console.log(error);
+            response.status(400).send({error:'malformated: id'})
+        })
     })
-
-    app.put('/api/notes/:id', (request, response) => {
+    app.delete('/api/persons/:id',(request,response) => {
+        Contact.findByIdAndDelete(request.params.id)
+        .then(() => {
+            response.status(204).end(); // Devuelve una respuesta sin contenido (No Content)
+        })
+        .catch(error => {
+            console.error(error);
+            response.status(400).send({ error: 'malformated: id' });
+        })
+    })
+ 
+    app.post('/api/persons',(request,response) => { //agrega notas
         const body = request.body
-        const note = {
-            content: body.content,
-            important: body.important
+        console.log(body)
+        if (!body.name || !body.number) {
+            console.log("error post")
+            return response.status(400).json({
+                error: 'nombre o numero faltante'
+            });
         }
-        Note.findByIdAndUpdate(request.params.id,note,{new:true})
+        const person = new Contact ({
+            name: body.name,
+            number: body.number
+        });
+        person.save().then(result => response.json(person))
+    })
+ 
+    app.put('/api/persons/:id', (request, response) => {
+        const body = request.body;
+    
+        // Verificar que los campos 'name' y 'number' estén presentes en el cuerpo de la solicitud
+        if (!body.name || !body.number) {
+            console.log("Error put")
+            return response.status(400).json({
+                error: 'Falta el nombre o numero'
+            });
+        }
+        // Crear un objeto con los datos actualizados
+        const person = {
+            name: body.name,
+            number: body.number 
+        }
+        Contact.findByIdAndUpdate(request.params.id,person,{new:true})
             .then(result => {
                 response.json(result)
             })
-            .catch(error => next(error))
     })
 
+ 
     const PORT = process.env.PORT
     app.listen(PORT, () => {
         //console.log(`Server express running on port ${PORT}`);
     })
+    
